f06f2ba932556a3310e7a0014b6d2c61f80c3eae
Aminjon <aminjonshermatov@gmail.com>
2020-11-28 17:04:25 +0500

    congrats

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const surname = document.querySelector('#surname');
+const birthDate = document.querySelector('#birthDate');
+const select = document.querySelector('#select');
+
+const resultObj = {
+    name: '',
+    surname: '',
+    birthDate: '',
+    select: ''
+};
+
+form.addEventListener('submit', ev => {
+    ev.preventDefault();
+    resultObj.name = name.value;
+    resultObj.surname = surname.value;
+    resultObj.birthDate = birthDate.value;
+    resultObj.select = select.value;
+
+    console.log(resultObj);
+});
